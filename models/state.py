2328aba57fdfcf42df2dfe1737ae763fe91301b3
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-03-17 11:17:06 -0400

    Update State: (models/state.py)

--- models/state.py
+++ models/state.py
@@ -1,22 +1,5 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship, backref
-from sqlalchemy.ext.declarative import declarative_base
-
-class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = 'states'
-
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete", backref="state")
-
-
-
-#!/usr/bin/python3
-""" State Module for HBNB project """
 from models.base_model import BaseModel
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
