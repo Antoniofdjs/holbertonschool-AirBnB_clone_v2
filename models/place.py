cf9b4ad7253d6e8ce4eab9dad1616a339f151652
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-03-18 22:28:03 -0400

    Update Place: (models/place.py)

--- models/place.py
+++ models/place.py
@@ -1,26 +1,5 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
-from sqlalchemy.orm import relationship
-
-class Place(BaseModel):
-    """ A place to stay """
-    __tablename__ = 'places'
-
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
-
-    user = relationship("User", back_populates="places")
-    city = relationship("City", back_populates="places")
 
 
 from models.base_model import BaseModel, Base
@@ -50,4 +29,3 @@ class Place(BaseModel, Base):
                             Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
 
     amenities = relationship("Amenity", secondary="place_amenity", viewonly=False)
-
