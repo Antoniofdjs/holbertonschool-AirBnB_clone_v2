5a677c71e5b432e9242dc99937b9594fbee1f48a
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-03-19 20:40:48 -0400

    .

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""This module instantiates an object of class FileStorage"""
-
-from os import getenv
-
-if getenv("HBNB_TYPE_STORAGE") == "db":
-    from models.engine.db_storage import DBStorage
-    storage = DBStorage()
-else:
-    from models.engine.file_storage import FileStorage
-=======
 """BACKUP VERSIOON"""
 
 
@@ -22,7 +11,6 @@ if storage == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
->>>>>>> master
     storage = FileStorage()
 
 storage.reload()
