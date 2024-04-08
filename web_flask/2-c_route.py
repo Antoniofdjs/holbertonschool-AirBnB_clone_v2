c93bc98e08a3af6cd903bca3504e1cf516138d66
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-04-08 16:36:08 -0400

    c route added

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+# Route to display "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def route(text="is cool"):    
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
