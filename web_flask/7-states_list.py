129ae8a8fae59e63454dd7040a90fcf9809377fe
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-04-12 19:29:31 -0400

    adding teardown in main app and if condition with jinja to check if states exists

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -89,5 +89,13 @@ def display_states():
     return render_template('7-states_list.html', states=states)
 
 
+@app.teardown_appcontext
+def teardown(exception=None):
+    '''
+        Cleanup
+    '''
+    storage.close()
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
